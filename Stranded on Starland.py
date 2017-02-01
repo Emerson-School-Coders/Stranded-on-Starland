62bd450efaee403782311c06db123b6b31245dbf
ndspikmin <ndstotlar@icloud.com>
2017-02-01 11:46:45 -0500

    Junge Area Done!
    Need to fix some bugs but almost done

--- Stranded on Starland.py
+++ Stranded on Starland.py
@@ -1,10 +1,9 @@
 import time
-import os
 global name
-def clear():
-    os.system("clear")
-    os.system("cls")
+global sword
+
 def start():
+    alive = 1
     print("Hello! to start this game we need to know your name.")
     time.sleep(3)
     name = input("What's your name?")
@@ -59,26 +58,118 @@ def start():
         time.sleep(6)
         jungle()
 
-def jungle()
-        print("A man greets you from a little hut, he says that it is a bad idea because there are many dangerous animals in the jungle but you continue at your own risk")
+def jungle():
+        print("You have entered the Junge Area!") 
         time.sleep(2)
         print("You are walking down the path when there is a fork in the road, go right or go left.")
         fork = ""
-        while fork!="right" and fork!="left"
-    
+        while fork!="right" and fork!="left":
+            fork = input("left/right: ")
+            if fork == "left":
+                print("You walk down the left path and find a huge treehouse!")
+                time.sleep(3)
+                print("A little boy runs out of the treehouse and takes you inside, he says its to dengerous aroud here and you should head back")
+                time.sleep(5)
+                print("You explain your ship crashed and that you had to come here to get the next power gem.")
+                time.sleep(3)
+                print("He says that if you continue you will eventually encounter a dangerous beast.")
+                time.sleep(3)
+                print("You leave the house and continue at your own risk.")
+                time.sleep(2)
+                print("As you are walking you see a child down at the river.")
+                river = ""
+                while river!="check" and river!="dont" and river!="don't":
+                    river = input("check/dont: ")
+                    if river == "check":
+                        print("You walk up to the child but it was an illusion!")
+                        time.sleep(3)
+                        print("The monster turns around and kills you.")
+                        time.sleep(3)
+                        print("Game Over.")
+                        time.sleep(3)
+                        print("The game will automatically start from where you were.")
+                        time.sleep(2)
+                        alive = 0
+                        jungle()
 
-        
-while 1:
-    clear()
-    whattodo=''
-    while whattodo!='p' and whattodo!='e':
-        print("Welcome to Stranded on Starland!")
-        print("Type p to play")
-        print("Type e to exit")
-        whattodo=input()
-        clear()
-    if whattodo=='p':
-        start()
-    else:
-        exit()
-    
+                    else:
+                        print("You continue to walk down the path.")
+                        time.sleep(3)
+                        print("When you reach the end of the path you see a chest.")
+                        time.sleep(2)
+                        print("Loot it or turn back?")
+                        sword = ""
+                        while sword!="loot" and sword!="turnback":
+                            sword = input("loot/turnback: ")
+                        if sword == "loot":
+                           print("You loot the chest and hey! A sword! Could be helpful later.")
+                           time.sleep(3)
+                           print("You head back to the fork in the road because there is no more things to do on the left path.")
+                           time.sleep(2)
+                           print("You head down the right path and it looks way longer than the left.")
+                           time.sleep(5)
+                           print("You have been walking for quite awhile when you get another call.")
+                           time.sleep(3)
+                           print("You pick up and it is Andrew on the line and he says that you are getting close to the Gem.")
+                           time.sleep(2)
+                           print("You hear a creepy growl from the bushes")
+                           time.sleep(2)
+                           print("As you aproach the bushes huge spider comes out and approaches you.")
+                           time.sleep(2)
+                           print("Good thing you found the sword.")
+                           time.sleep(2)
+                           print("You slay the beast and he drops the second power gem.")
+                           time.sleep(2)
+                           print("Andrew says you should head back imediately. So you go as fast as you can.")
+
+                           if sword == "turnback":
+                               print("It was probably a trap anyway.")
+                               time.sleep(3)
+                               print("You head back to the fork in the road because there is no more things to do on the left path.")
+                               time.sleep(2)
+                               print("You head down the right path and it looks way longer than the left.")
+                               time.sleep(5)
+                               print("You have been walking for quite awhile when you get another call.")
+                               time.sleep(3)
+                               print("You pick up and it is Andrew on the line and he says that you are getting close to the Gem.")
+                               time.sleep(2)
+                               print("You hear a creepy growl from the bushes")
+                               time.sleep(2)
+                               print("As you aproach the bushes huge spider comes out and approaches you.")
+                               time.sleep(2)
+                               print("You don't have anything to defend yourself with.")
+                               time.sleep(3)
+                               print("The huge spider slays you.")
+                               time.sleep(3)
+                               print("Game Over.")
+                               time.sleep(3)
+                               print("The game will automatically start from where you were.")
+                               time.sleep(2)
+                               alive = 0
+                               jungle()
+                           
+
+                        if fork == "right":
+                            print ("You head down the right path.")
+                            time.sleep(5)
+                            print("You have been walking for quite awhile when you get another call.")
+                            time.sleep(3)
+                            print("You pick up and it is Andrew on the line and he says that you are getting close to the Gem.")
+                            time.sleep(2)
+                            print("You hear a creepy growl from the bushes")
+                            time.sleep(2)
+                            print("As you aproach the bushes huge spider comes out and approaches you.")
+                            time.sleep(2)
+                            print("You don't have anything to defend yourself with.")
+                            time.sleep(3)
+                            print("The huge spider slays you.")
+                            time.sleep(3)
+                            print("Game Over.")
+                            time.sleep(3)
+                            print("The game will automatically start from where you were.")
+                            time.sleep(2)
+                            alive = 0
+                            jungle()
+                            
+                        
+start()
